821ea27abd055082f1d899659e021a701d0c14dd
hassanrehman01398 <hassanrehman01398@gmail.com>
2020-08-23 01:31:16 +0500

    talha changes

--- src/Components/PostCard.js
+++ src/Components/PostCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './talha.css';
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
